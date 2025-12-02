45b76f8d591f6cb127da868168591a43e4137cdd
Rynboss <raiyanahammad392@gmail.com>
2025-12-02 10:38:39 +0600

    Clean up adieu.py by removing blank lines
    Removed unnecessary blank lines for cleaner code.

--- adieu/adieu.py
+++ adieu/adieu.py
@@ -1,12 +1,7 @@
 import inflect
-
 p = inflect.engine()
-
-
 def main():
     sing_adieu()
-
-
 def sing_adieu():
     names = []
     try:
@@ -14,7 +9,5 @@ def sing_adieu():
             names.append(input("Input: "))
     except EOFError:
         print("\nAdieu, adieu, to", p.join(names))
-
-
 if __name__ == "__main__":
     main()
