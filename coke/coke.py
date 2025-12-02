21636bb0c5a5b71b246e7a191369185540de4df6
Rynboss <raiyanahammad392@gmail.com>
2025-12-02 10:39:23 +0600

    Update coke.py

--- coke/coke.py
+++ coke/coke.py
@@ -1,7 +1,5 @@
 def main():
     change_calc()
-
-
 def change_calc():
     amount_due = 50
     accepted_coins = [25, 10, 5]
@@ -12,7 +10,5 @@ def change_calc():
             amount_due -= user_coin
         if amount_due <= 0:
             print(f"Change Owed: {amount_due * -1}")
-
-
 if __name__ == "__main__":
     main()
